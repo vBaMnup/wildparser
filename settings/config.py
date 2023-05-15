d409f7c186bc55914174283df39b181f0e82c35e
vbamnup <vbamnup@gmail.com>
2023-05-15 15:57:01 +0300

    Fix env file

--- settings/config.py
+++ settings/config.py
@@ -8,16 +8,16 @@ HEADERS = {
     "Accept-Language": "ru-RU,ru;q=0.9,en-US;q=0.8,en;q=0.7",
     "Cache-Control": "no-cache",
     "Connection": "keep-alive",
-    "Origin": environ.get("Origin"),
+    "Origin": environ.get("ORIGIN"),
     "Pragma": "no-cache",
-    "Referer": environ.get("Referer"),
+    "Referer": environ.get("REFERER"),
     "Sec-Fetch-Dest": "empty",
     "Sec-Fetch-Mode": "cors",
     "Sec-Fetch-Site": "cross-site",
-    "User-Agent": environ.get("User-Agent"),
-    "sec-ch-ua": environ.get("sec-ch-ua"),
+    "User-Agent": environ.get("USERAGENT"),
+    "sec-ch-ua": environ.get("SECCHUA"),
     "sec-ch-ua-mobile": "?0",
-    "sec-ch-ua-platform": environ.get("sec-ch-ua-platform"),
+    "sec-ch-ua-platform": environ.get("SECCHUAPLATFORM"),
 }
 
 DELAY = int(environ.get("DELAY"))
